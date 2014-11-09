9f7c3bac9b7a5bca7e30c0f45b5c9e75aa44ab61
Andy Loughran <aloughran@scorecomms.com>
2014-11-09 19:52:42 +0000

    Initial commit

---
+++ app/models/inventory.server.model.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Inventory Schema
+ */
+var InventorySchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Inventory name',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	picture: {
+		type: String,
+		default: '',
+		updated: { type: Date, default: Date.now }
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	},
+	categories: {
+		type: [String],
+		index: true,
+		ref: 'Category'
+	}
+});
+
+mongoose.model('Inventory', InventorySchema);
